b05932640dce1afabb5db789ddb4941fb1450ae3
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-03-21 15:36:40 +0200

    email.js update

---
+++ assets/js/email.js
@@ -0,0 +1,40 @@
+const form = document.querySelector(".php-email-form");
+
+const submitButton = document.querySelector(
+  ".php-email-form button[type=submit]"
+);
+
+const postData = async (data) => {
+  var res = await fetch("http://localhost:3000/tich", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  res = await res.json();
+  return res;
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+submitButton.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const name = form[0].value;
+  const email = form[1].value;
+  const subject = form[2].value;
+  const message = form[3].value;
+
+  if (!name || !email || !subject || !message) {
+    alert("Please fill out all fields");
+    return;
+  }
+
+  const data = { name, email, subject, message };
+  const res = await postData(data);
+  alert(res);
+});
